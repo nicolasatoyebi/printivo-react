03aabb6d12e18bf146d8c70160c900d128848de3
nicolas <nicolasatoyebi@gmail.com>
2024-05-20 21:07:55 +0100

    first commit

---
+++ src/component/Nav.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <div>
+        <div>
+            <ul>
+                <li><a href="#">Cost Calculator</a></li>
+                <li><a href="#">Cost Calculator</a></li>
+                <li><a href="#">Cost Calculator</a></li>
+            </ul>
+        </div>
+        <div></div>
+    </div>
+  )
+}
+
+export default nav
